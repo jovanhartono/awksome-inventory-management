672cb2a9d6d092edffab2a8c726cf10b94ed5fef
jovan <jovanus@yahoo.co.id>
2022-09-10 12:17:55 +0700

    added const base products

--- lib/seed.ts
+++ lib/seed.ts
@@ -9,3 +9,75 @@ export const variants = [
         name: 'PRO'
     },
 ]
+
+export const products = [
+    {
+        name: 'Chef kirby'
+    },
+    {
+        name: 'Snorlax'
+    },
+    {
+        name: 'Marshall'
+    },
+    {
+        name: 'Jar of Skippy'
+    },
+    {
+        name: 'Sinchan'
+    },
+    {
+        name: 'Cookie Bear'
+    },
+    {
+        name: 'Jimbo Baby Boss'
+    },
+    {
+        name: 'Banana'
+    },
+    {
+        name: 'Cup Noodle'
+    },
+    {
+        name: 'Faceless'
+    },
+    {
+        name: 'Flat Corgi'
+    },
+    {
+        name: 'Flat Mouse'
+    },
+    {
+        name: 'Flat Dino'
+    },
+    {
+        name: 'Flat Duck'
+    },
+    {
+        name: 'Hoodie Shark'
+    },
+    {
+        name: 'Bear Brick'
+    },
+    {
+        name: 'Picnic Duck'
+    },
+    {
+        name: 'Squidward'
+    },
+    {
+        name: 'Slingbag Duck'
+    },
+    {
+        name: 'Mushroom Toad'
+    },
+    {
+        name: 'Starbucks'
+    },
+    {
+        name: 'Corgi Butt'
+    },
+    {
+        name: 'Brown Bear'
+    }
+]
