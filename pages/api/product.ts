06b3def7a06429cb913647855f178848163fd751
jovan <jovanus@yahoo.co.id>
2022-10-07 16:56:32 +0700

    created api endpoint and logic for inserting product details

--- pages/api/product.ts
+++ pages/api/product.ts
@@ -1,11 +1,36 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next'
-import {prisma} from"../../prisma/config";
+import {prisma} from "../../prisma/config";
+import {nanoid} from "nanoid";
+import {ProductInputForm} from "../../types/dto";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-    if (request.method === 'GET'){
-        const products = await prisma.product.findMany();
+    if (request.method === 'POST') {
+        const {name, details}: ProductInputForm = request.body as ProductInputForm;
+        const productId = `PR-${nanoid(5)}`;
 
-        response.status(200).json(products);
+        const product = await prisma.product.create({
+            data: {
+                id: productId,
+                name,
+                productDetail: {
+                    create: details.map(({qty, variantId, price}) => {
+                        return {
+                            id: `PRD-${nanoid(5)}`,
+                            qty,
+                            price,
+                            variant: {
+                                connect: {
+                                    id: variantId
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        });
+
+        response.status(200).json(product);
+    } else {
+        response.status(404).send('Method not allowed!');
     }
 }
