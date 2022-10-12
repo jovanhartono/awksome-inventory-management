8e50206987bf517f573b9035d59a8894f39a50ef
jovan <jovanus@yahoo.co.id>
2022-10-12 09:29:01 +0700

    added dynamic route for product detail and it's api

--- pages/api/product/[id].ts
+++ pages/api/product/[id].ts
@@ -1,8 +1,65 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {prisma} from "prisma/config";
 
-export default function handler (request: NextApiRequest, response: NextApiResponse) {
-    const {id: productId} = request.query as {id: string};
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+    const {id: productId} = request.query as { id: string };
     console.log(productId);
+
+    const changes = [
+        {
+            id: '1',
+            price: 10000,
+            qty: 1
+        },
+        {
+            id: '2',
+            price: 20000,
+            qty: 2
+        },
+        {
+            id: '3',
+            price: 30000,
+            qty: 3
+        }
+    ]
+
+    if (request.method === 'PUT') {
+        await prisma.product.update({
+            where: {
+                id: productId
+            },
+            data: {
+                name: 'updated name',
+                productDetail: {
+                    deleteMany: {
+                        id: {notIn: ['1', '2', '3']}
+                    },
+                    upsert: changes.map((value) => {
+                        return {
+                            where: {
+                                id: value.id
+                            },
+                            create: {
+                                price: 10,
+                                qty: 5,
+                                variant: {
+                                    connect: {
+                                        id: '1'
+                                    }
+                                }
+                            },
+                            update: {
+                                qty: value.qty,
+                                price: value.price
+                            }
+                        }
+                    })
+                }
+            }
+        });
+
+        console.log('success');
+    }
 
     response.status(200).send('ok');
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,10 +1,46 @@
 import {NextRouter, useRouter} from "next/router";
 import axios from "../../lib/axios";
 import Head from "next/head";
+import {GetStaticPaths, GetStaticProps} from "next";
+import {prisma} from "prisma/config";
+import {Variant} from '@prisma/client';
+import {useFieldArray, useForm} from "react-hook-form";
+import TextField from "../../components/text-field";
+import {zodResolver} from "@hookform/resolvers/zod";
 
-export default function ProductDetail() {
+type ProductProps = {
+    id: string,
+    name: string,
+    productDetail: {
+        variant: Variant,
+        qty: number
+    }[]
+}
+
+type ProductForm = Omit<ProductProps, "id">;
+
+type ProductDetailPageProps = {
+    product: ProductProps
+}
+
+export default function ProductDetail({product}: ProductDetailPageProps) {
+    const {register, handleSubmit, formState: {errors}, control} = useForm<ProductForm>({
+        defaultValues: {
+            name: product.name,
+            productDetail: product.productDetail
+        },
+    });
+    const {fields, append, remove} = useFieldArray({
+        name: 'productDetail',
+        control
+    })
+
     const {query}: NextRouter = useRouter();
-    const {id}: { id: string } = query as { id: string };
+    const {id} = query as { id: string };
+
+    const onSubmit = (data: ProductForm) => {
+        console.log(data);
+    }
 
     return (
         <>
@@ -12,13 +48,93 @@ export default function ProductDetail() {
                 <title>Detail</title>
             </Head>
             <section>
-                <button className="button-small" onClick={async () => {
-                    const res = await axios.get(`/product/${id}`);
-                    console.log(res);
-                }}>
-                    fetch {id}
-                </button>
+                <form onSubmit={handleSubmit(onSubmit)}
+                      className={"space-y-3"}
+                >
+                    <TextField label={'Product Name'} {...register("name")}/>
+                    {
+                        fields.map((field, index: number) => {
+                            return (
+                                <div key={field.id}>
+                                    <p>{field.variant.name}</p>
+                                    <p>{field.qty}</p>
+                                </div>
+                            )
+                        })
+                    }
+
+                    <button className="button-submit"
+                            onClick={() => {
+
+                            }}
+                    >
+                        Update
+                    </button>
+                </form>
+                {
+                    product.productDetail.map((detail, index: number) => {
+                        return (
+                            <div key={index}>
+                                <span>qty: {detail.qty}</span>
+                                <span>variant :{detail.variant.name}</span>
+                            </div>
+                        )
+                    })
+                }
+
             </section>
         </>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const ids: Array<{ id: string }> = await prisma.product.findMany({
+        select: {
+            id: true
+        }
+    });
+
+    const paths = ids.map(({id}) => {
+        return {
+            params: {
+                id
+            }
+        }
+    })
+
+    return {
+        paths,
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    const id = params?.id as string;
+    const product: ProductProps | null = await prisma.product.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true,
+            productDetail: {
+                select: {
+                    variant: true,
+                    qty: true,
+                }
+            }
+        }
+    })
+
+    if (!product) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            product
+        }
+    }
 }
