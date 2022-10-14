3f86fbb132444b2fa4a66a6a4e8aa5acbd81171b
jovan <jovanus@yahoo.co.id>
2022-10-14 12:27:17 +0700

    try bug fixes scroll on ios safari (3)

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -36,7 +36,7 @@ export default function Dialog(dialogProps: DialogProps) {
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto overflow-y-scroll">
+        <div className="fixed inset-0 overflow-y-scroll touch-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
