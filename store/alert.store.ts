1503cabfc31ec79c56376cfbf1d99992d3ba9cce
jovan <jovanus@yahoo.co.id>
2022-10-21 14:26:18 +0700

    adding state management for global alert component

---
+++ store/alert.store.ts
@@ -0,0 +1,41 @@
+import create from "zustand";
+import produce from "immer";
+
+export enum AlertStatus {
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
+type AlertPayload = {
+  visible: boolean;
+  message: string;
+  status: AlertStatus;
+};
+
+type AlertStore = {
+  alertState: AlertPayload;
+  show: (message: string, status: AlertStatus) => void;
+  hide: () => void;
+};
+
+export const useAlertStore = create<AlertStore>((set) => ({
+  alertState: {
+    visible: false,
+    message: "",
+    status: AlertStatus.SUCCESS,
+  },
+  show: (message: string, status: AlertStatus) =>
+    set(
+      produce(({ alertState, hide }: AlertStore) => {
+        alertState.visible = true;
+        alertState.status = status;
+        alertState.message = message;
+
+        setTimeout(() => {
+            hide();
+        }, 3000)
+      })
+    ),
+  hide: () =>
+    set(produce((state: AlertStore) => void(state.alertState.visible = false))),
+}));
