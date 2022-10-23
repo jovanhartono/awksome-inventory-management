a0ea2c217c23792be1b025f39e08f1d2664a9ab6
jovan <jovanus@yahoo.co.id>
2022-10-23 21:05:29 +0700

    alert component now use transition

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,17 +1,29 @@
 import { AlertStatus, useAlertStore } from "../store/alert.store";
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 export default function Alert() {
   const {
     alertState: { status, message, visible },
   } = useAlertStore();
 
-  return visible ? (
-      <div className="fixed top-1.5 ">
+  return (
+    <Transition
+      as={Fragment}
+      show={visible}
+      enter="duration-200 transform transition ease-in-out"
+      enterFrom="-translate-y-6 opacity-0"
+      enterTo="translate-y-0 opacity-100"
+      leave="duration-200 transform transition ease-in-out"
+      leaveFrom="translate-y-0 opacity-100"
+      leaveTo="-translate-y-6 opacity-0"
+    >
+      <div className="fixed top-6 flex justify-center left-0 right-0 px-6 md:px-0">
         <div
           className={`${status === AlertStatus.SUCCESS && "bg-green-100"}
-                       ${status === AlertStatus.ERROR && "bg-red-100"}
-                        rounded p-3 flex items-center space-x-3 mx-auto`}
+                           ${status === AlertStatus.ERROR && "bg-red-100"}
+                            rounded p-3 flex items-center space-x-3 container`}
         >
           {status === AlertStatus.SUCCESS && (
             <CheckIcon className={"w-5 h-5 text-green-700"} />
@@ -21,14 +33,13 @@ export default function Alert() {
           )}
           <span
             className={`${status === AlertStatus.SUCCESS && "text-green-700"} 
-                        ${status === AlertStatus.ERROR && "text-red-700"} 
-                        font-medium`}
+                            ${status === AlertStatus.ERROR && "text-red-700"} 
+                            font-medium`}
           >
             {message}
           </span>
         </div>
       </div>
-  ) : (
-    <></>
+    </Transition>
   );
 }
