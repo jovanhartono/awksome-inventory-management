0c7c1aa070179c7157e68a328d1eb1c929755514
jovan <jovanus@yahoo.co.id>
2022-10-31 17:01:29 +0700

    added bulk variant endpoint

--- pages/api/variant.ts
+++ pages/api/variant.ts
@@ -1,12 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../prisma/config";
+import { Variant as PrismaVariant } from "@prisma/client";
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   if (request.method === "GET") {
-    const variants = await prisma.variant.findMany();
+    const variants: PrismaVariant[] = await prisma.variant.findMany({
+      orderBy: {
+        name: "asc",
+      },
+    });
 
     response.status(200).json(variants);
   }
