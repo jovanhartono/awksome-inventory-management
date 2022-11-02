112f948128343fb3aa9cc36710ef9787814cb47e
jovan <jovanus@yahoo.co.id>
2022-11-02 14:06:43 +0700

    order page now use client side data fetching, table for order preview, and add RHF for handling form

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,59 +1,114 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import dayjs from "dayjs";
-import Dropdown from "../components/dropdown";
-import { ChangeEvent, FormEvent, Fragment, useEffect, useState } from "react";
-import { PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { prisma } from "../prisma/config";
+import ListBox from "components/listBox";
+import { useEffect, useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import {
   Product as PrismaProduct,
   Variant as PrismaVariant,
 } from "@prisma/client";
-import { nanoid } from "nanoid";
-
-type ProductDetail = {
-  key?: string;
-  productId: string;
-  variantId: string;
-  qty: string;
-};
-
-type OrderProps = {
-  products: Array<ProductDropdown>;
-  variants: Array<PrismaVariant>;
-};
+import { useForm, useWatch } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import TextField from "components/text-field";
+import { useProduct, useVariant } from "@hooks";
+import produce from "immer";
+import { AlertStatus, useAlertStore } from "store/alert.store";
+import { TrashIcon } from "@heroicons/react/20/solid";
 
 type ProductDropdown = Omit<PrismaProduct, "updatedAt">;
 
-const Order: NextPage<OrderProps> = ({ products, variants }: OrderProps) => {
-  const baseProduct: ProductDetail = {
-    productId: products[0].id.toString(),
-    qty: "1",
-    variantId: variants[0].id.toString(),
-  };
-  // dayjs unix value is floored into the nearest second. so we need to multiply into milliseconds.
-  const [orderDate, setOrderDate] = useState<number>(dayjs().unix() * 1000);
-  const [productDetails, setProductDetails] = useState<ProductDetail[]>([]);
+type OrderDetail = {
+  productId: string;
+  productLabel: string;
+  variantId: string;
+  variantLabel: string;
+  qty: number;
+};
+
+type OrderForm = {
+  date: Date;
+  orderDetail: OrderDetail;
+};
+
+const schema = z.object({
+  date: z.preprocess((arg) => {
+    if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+  }, z.date({ invalid_type_error: "Must be type of Date!" })),
+  orderDetail: z.object({
+    qty: z
+      .number({ invalid_type_error: "Qty must be a positive number!" })
+      .positive(),
+    variantId: z.string(),
+    variantLabel: z.string(),
+    productId: z.string(),
+    productLabel: z.string(),
+  }),
+});
+
+const Order: NextPage = () => {
+  const { show: showAlert } = useAlertStore();
+
+  const { products, isLoading: isProductLoading } = useProduct();
+  const { variants, isLoading: isVariantLoading } = useVariant();
+
+  const [orderDetail, setOrderDetail] = useState<OrderDetail[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm<OrderForm>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      date: new Date(),
+      orderDetail: {
+        qty: 1,
+      },
+    },
+  });
+
+  const date = useWatch({
+    control,
+    name: "date",
+  });
 
   useEffect(() => {
-    setProductDetails([{ ...baseProduct, key: nanoid(10) }]);
-  }, []);
+    if (products.length > 0 && variants.length > 0) {
+      setValue("orderDetail.productId", products[0].id);
+      setValue("orderDetail.productLabel", products[0].name);
+      setValue("orderDetail.variantId", variants[0].id);
+      setValue("orderDetail.variantLabel", variants[0].name);
+    }
+  }, [products, variants]);
 
-  const handleProductDetails = (
-    value: string,
-    index: number,
-    key: keyof ProductDetail
-  ) => {
-    setProductDetails((currentValue: ProductDetail[]) => {
-      const newValue = currentValue;
-      newValue[index][key] = value;
-      return [...newValue];
+  function submitForm(data: OrderForm): void {
+    const isDuplicate = orderDetail.some((item: OrderDetail) => {
+      return (
+        data.orderDetail.productId === item.productId &&
+        data.orderDetail.variantId === item.variantId
+      );
     });
-  };
 
-  function submitForm(event: FormEvent): void {
-    event.preventDefault();
-    console.log(orderDate, productDetails);
+    if (isDuplicate) {
+      showAlert("Cannot add duplicate product", AlertStatus.ERROR);
+      return;
+    }
+
+    setOrderDetail(
+      produce((draft) => {
+        draft.push(data.orderDetail);
+      })
+    );
+  }
+
+  if (isProductLoading || isVariantLoading) {
+    return <p>Loading...</p>;
   }
 
   return (
@@ -61,160 +116,140 @@ const Order: NextPage<OrderProps> = ({ products, variants }: OrderProps) => {
       <Head>
         <title>Order Page</title>
       </Head>
-      <section>
+      <section className={"space-y-3"}>
         <div className="flex justify-between items-center">
           <h2>Add Order</h2>
           <p className="leading-none font-light text-sm text-gray-500">
-            {dayjs().format("dddd, D MMMM YYYY")}
+            {dayjs(date).format("dddd, DD MMMM YYYY")}
           </p>
         </div>
-        <div className="pt-3">
-          <form className={"space-y-3"} onSubmit={submitForm}>
-            <div>
-              <label htmlFor={"order-date"}>Date</label>
-              <input
-                id={"order-date"}
-                type="date"
-                value={dayjs(orderDate).format("YYYY-MM-DD")}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  setOrderDate(dayjs(event.target.value).unix() * 1000);
+        <form className={"space-y-3"} onSubmit={handleSubmit(submitForm)}>
+          <div>
+            <label htmlFor={"order-date"}>Date</label>
+            <input
+              id={"order-date"}
+              type="date"
+              {...register("date")}
+              defaultValue={dayjs(new Date()).format("YYYY-MM-DD")}
+            />
+          </div>
+          <div className="space-y-3">
+            <div className="flex justify-between items-center">
+              <label className="mb-0">Products</label>
+              <button className="button-small" type={"submit"}>
+                <PlusIcon className="w-4 h-4 text-gray-500 mr-2" />
+                Add Products
+              </button>
+            </div>
+            <ListBox
+              value={watch("orderDetail.productId")}
+              options={products.map((product: ProductDropdown) => {
+                return {
+                  label: product.name,
+                  value: product.id,
+                };
+              })}
+              onChange={({ value, label }) => {
+                setValue("orderDetail.productId", value);
+                setValue("orderDetail.productLabel", label);
+              }}
+            />
+          </div>
+          <div className={"grid grid-cols-2 gap-3"}>
+            <div className={"space-y-1"}>
+              <label htmlFor={"order-variant"} className={"mb-0"}>
+                Variants
+              </label>
+              <ListBox
+                value={watch("orderDetail.variantId")}
+                options={variants.map((variant: PrismaVariant) => {
+                  return {
+                    label: variant.name,
+                    value: variant.id,
+                  };
+                })}
+                onChange={({ value, label }) => {
+                  setValue(`orderDetail.variantId`, value);
+                  setValue(`orderDetail.variantLabel`, label);
                 }}
               />
             </div>
-            {productDetails.map(
-              (productDetail: ProductDetail, index: number) => {
-                return (
-                  <Fragment key={productDetail.key}>
-                    <div className="space-y-3">
-                      <div className="flex justify-between items-center">
-                        <label className="mb-0">Products</label>
-                        {index === 0 ? (
-                          <button
-                            className="button-small"
-                            onClick={() => {
-                              setProductDetails(
-                                (currentValue: ProductDetail[]) => {
-                                  const newValue = [
-                                    ...currentValue,
-                                    {
-                                      ...baseProduct,
-                                      key: nanoid(10),
-                                    },
-                                  ];
-                                  return [...newValue];
-                                }
-                              );
-                            }}
-                          >
-                            <PlusIcon className="w-4 h-4 text-gray-500 mr-2" />
-                            Add Products
-                          </button>
-                        ) : (
-                          <div
-                            className="basic-transition hover:bg-red-100 cursor-pointer rounded-full p-2 bg-red-50 flex items-center justify-center"
-                            onClick={() =>
-                              setProductDetails(
-                                (currentValue: ProductDetail[]) => {
-                                  const newValue = [...currentValue];
-                                  return newValue.filter(
-                                    (_, idx: number) => idx !== index
-                                  );
-                                }
-                              )
-                            }
-                          >
-                            <TrashIcon className="w-4 h-4 text-red-500" />
-                          </div>
-                        )}
-                      </div>
-                      <Dropdown
-                        value={productDetails[index].productId}
-                        options={products.map((product: ProductDropdown) => {
-                          return {
-                            label: product.name,
-                            value: product.id,
-                          };
-                        })}
-                        changeHandler={({ value }) => {
-                          handleProductDetails(value, index, "productId");
-                        }}
-                      />
-                    </div>
-                    <div className={"grid grid-cols-2 gap-3"}>
-                      <div>
-                        <label htmlFor={"order-variant"}>Variants</label>
-                        <Dropdown
-                          options={variants.map((variant: PrismaVariant) => {
-                            return {
-                              label: variant.name,
-                              value: variant.id,
-                            };
-                          })}
-                          changeHandler={(data) => {
-                            handleProductDetails(
-                              data.value,
-                              index,
-                              "variantId"
-                            );
-                          }}
-                        />
-                      </div>
-                      <div>
-                        <label>Qty</label>
-                        <input
-                          id={"order-qty"}
-                          type="number"
-                          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                            const qty = event.target.value;
-                            const arrQty = qty.split("");
-                            if (arrQty[0] === "0") {
-                              arrQty.shift();
-                            }
-                            handleProductDetails(qty, index, "qty");
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </Fragment>
-                );
-              }
-            )}
-            <div className="rounded bg-gray-100 p-4">
-              <h3>Preview</h3>
-              {}
-            </div>
-
             <div>
-              <button type="submit" className={"mt-3 button-submit"}>
-                Add Order
-              </button>
+              <TextField
+                label="Quantity"
+                {...register(`orderDetail.qty`, {
+                  valueAsNumber: true,
+                })}
+              />
+              {errors.orderDetail?.qty && (
+                <small className="text-sm font-light text-amber-700">
+                  {errors.orderDetail?.qty?.message}
+                </small>
+              )}
             </div>
-          </form>
+          </div>
+        </form>
+        <div className="p-4 rounded border">
+          <table className={"table-auto w-full"}>
+            <thead className={"uppercase text-sm"}>
+              <tr className={"bg-gray-50 text-left rounded-xl overflow-hidden"}>
+                {["Product", "Variant", "Qty", ""].map(
+                  (value: string, index: number) => {
+                    return (
+                      <th
+                        key={index}
+                        className={"p-3 font-medium text-gray-700"}
+                      >
+                        {value}
+                      </th>
+                    );
+                  }
+                )}
+              </tr>
+            </thead>
+            <tbody className={"text-sm divide-y divide-y-gray-50"}>
+              {orderDetail.map((detail: OrderDetail) => {
+                return (
+                  <tr key={`${detail.productId}${detail.variantId}`}>
+                    <td className={"p-3 text-amber-700"}>
+                      {detail.productLabel}
+                    </td>
+                    <td className={"p-3 font-light"}>{detail.variantLabel}</td>
+                    <td className={"p-3 font-light"}>{detail.qty}</td>
+                    <td className={"p-3 flex justify-center items-center"}>
+                      <div
+                        className="p-1.5 rounded-full bg-red-50 cursor-pointer"
+                        onClick={() => {
+                          setOrderDetail(
+                            produce((draft: OrderDetail[]) =>
+                              draft.filter(
+                                (item: OrderDetail) =>
+                                  item.variantId !== detail.variantId ||
+                                  item.productId !== detail.productId
+                              )
+                            )
+                          );
+                        }}
+                      >
+                        <TrashIcon className={"w-4 h-4 text-red-600"} />
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
+        <button
+          type="submit"
+          className={"mt-3 button-submit"}
+          onClick={() => setOrderDetail([])}
+        >
+          Add Order
+        </button>
       </section>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const products: ProductDropdown[] = await prisma.product.findMany({
-    select: {
-      name: true,
-      id: true,
-    },
-  });
-  const variants: PrismaVariant[] = await prisma.variant.findMany();
-
-  const mappedProducts: Array<ProductDropdown> = products.map(
-    ({ id, name }) => ({ id, name })
-  );
-
-  return {
-    props: {
-      products: mappedProducts,
-      variants,
-    },
-  };
 };
 
 export default Order;
