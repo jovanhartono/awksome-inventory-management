8cc70ebdd3aed6c285211ac051a7305dd97b83e0
jovan <jovanus@yahoo.co.id>
2023-02-10 15:04:10 +0700

    move order filter into separate endpoint

---
+++ pages/api/order/filter.ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { OrderGroup } from "types/prisma.types";
+import { prisma } from "prisma/config";
+import { Prisma } from "@prisma/client";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+import dayjs from "dayjs";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "GET") {
+    const { orderDateFrom, orderDateTo } = request.query;
+    const orderList: Array<OrderGroup> = await prisma.$queryRaw(
+      Prisma.sql`
+            SELECT DATE(CONVERT_TZ(o.createdAt, '+00:00', '+07:00')) as createdAt, p.name as productName, v.name as variant, COUNT(od.qty) as qty
+            FROM \`Order\` o 
+            JOIN OrderDetails od 
+            ON od.orderId = o.id 
+            JOIN ProductDetail pd 
+            ON od.productDetailsId = pd.id 
+            JOIN Product p 
+            ON p.id = pd.productId 
+            JOIN Variant v 
+            ON v.id = pd.variantId
+            WHERE createdAt BETWEEN ${orderDateFrom} AND ${orderDateTo}  
+            GROUP BY createdAt, p.name, v.name
+            ORDER BY createdAt DESC
+        `
+    );
+
+    const groupedData = orderList.reduce((acc, curr) => {
+      const date = dayjs(curr.createdAt).format("YYYY-MM-DD");
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push({
+        ...curr,
+        qty: Number(curr.qty),
+      });
+      return acc;
+    }, {} as { [key: string]: OrderGroup[] });
+
+    const groupedDataArray = Object.entries(groupedData).map(
+      ([date, data]) => ({
+        date,
+        data,
+      })
+    );
+
+    const convertedOrderList = groupedDataArray.map((order) => ({
+      orderDate: order.date,
+      data: order.data,
+    }));
+
+    response.status(200).send(convertedOrderList);
+  }
+}
