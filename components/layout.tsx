65f03abf4a463cc9667b5d55965970b278b38631
jovan <jovanus@yahoo.co.id>
2023-02-27 17:46:41 +0700

    modified layout for login page

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,12 +3,15 @@ import Footer from "components/nav/footer";
 import Loader from "components/feedback/loader";
 import Alert from "components/feedback/alert";
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
   return (
     <div className={"min-h-screen"}>
+      {router.pathname !== "/login" && <Header />}
       <Loader />
-      <Header />
       <Alert />
       <main className={"p-6"}>{children}</main>
       <Footer />
